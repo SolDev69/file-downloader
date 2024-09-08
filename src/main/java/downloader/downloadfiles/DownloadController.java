8234cd110a2b7d3fc3a76900fe431a5329cf5709
SolDev69 <michaelcraft1104@gmail.com>
2024-09-08 18:55:41 -0400

    updates, add cli functionality

--- src/main/java/downloader/downloadfiles/DownloadController.java
+++ src/main/java/downloader/downloadfiles/DownloadController.java
@@ -3,74 +3,91 @@ package downloader.downloadfiles;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.TextArea;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class DownloadController {
 
-
     @FXML
-    protected void initialize()
-    {
-
+    protected void initialize() {
     }
 
-
-    void download(String a, String b)
-    {
-        download(a,b,"");
+    void download(String a, String b) {
+        download(a, b, "");
     }
 
-    void download(String a, String b, String c)
-    {
+    void download(String a, String b, String subdirectory) {
         try {
-            for (int i = 1; i < 999; i++) {
+            // Determine the download directory
+            String baseDirectory = System.getProperty("user.dir") + "/OCTMaps" + (subdirectory.isEmpty() ? "" : "/" + subdirectory);
+            File directory = new File(baseDirectory);
+
+            // Create the directory if it doesn't exist
+            if (!directory.exists()) {
+                directory.mkdirs();
+            }
+
+            // Download the files
+            for (int i = 0; i < 1000; i++) {
                 String formattedNumber = String.format("%03d", i);
-                downloadFile(a + formattedNumber + b, formattedNumber, c);
+                downloadFile(a + formattedNumber + b, formattedNumber, baseDirectory);
             }
             showCompletionWindow();
-        } catch (IOException e)
-        {
+        } catch (IOException e) {
             System.err.println("Something went wrong!\n");
             e.printStackTrace();
         }
     }
+
     @FXML
-    protected void downloadCurrent()
-    {
-        download("https://www.octranspo.com/images/files/routes_pdf/map_carte_", ".pdf");
+    protected void downloadCurrent() {
+        download("https://www.octranspo.com/images/files/routes_pdf/map_carte_", ".pdf", "current");
     }
 
     @FXML
-    protected void downloadNWTB()
-    {
-        download("https://www.octranspo.com/images/files/files/routes_pdf_future/RD_Map_", "_(Jan2024).pdf");
-
+    protected void downloadNWTB() {
+        download("https://www.octranspo.com/images/files/files/routes_pdf_future/RD_Map_", "_(Jan2024).pdf", "NWTB");
     }
 
     @FXML
-    protected void downloadOlder()
-    {
-        download("https://www.octranspo.com/images/files/files/routes_pdf/map_carte_", ".pdf");
-    }
-
-    @FXML
-    protected void downloadAll()
-    {
-        download("https://www.octranspo.com/images/files/routes_pdf/map_carte_", ".pdf");
-        download("https://www.octranspo.com/images/files/files/routes_pdf_future/RD_Map_", "_(Jan2024).pdf");
+    protected void downloadOlder() {
         download("https://www.octranspo.com/images/files/files/routes_pdf/map_carte_", ".pdf", "older");
-
     }
 
+    @FXML
+    protected void downloadAll() {
+        ExecutorService executor = Executors.newFixedThreadPool(3); // Create a pool of 3 threads
+
+        // Submit each download task to the executor
+        executor.submit(() -> {
+            System.out.println("Starting download for current maps...");
+            download("https://www.octranspo.com/images/files/routes_pdf/map_carte_", ".pdf", "current");
+            System.out.println("Finished downloading current maps.");
+        });
+
+        executor.submit(() -> {
+            System.out.println("Starting download for NWTB maps...");
+            download("https://www.octranspo.com/images/files/files/routes_pdf_future/RD_Map_", "_(Jan2024).pdf", "NWTB");
+            System.out.println("Finished downloading NWTB maps.");
+        });
+
+        executor.submit(() -> {
+            System.out.println("Starting download for older maps...");
+            download("https://www.octranspo.com/images/files/files/routes_pdf/map_carte_", ".pdf", "older");
+            System.out.println("Finished downloading older maps.");
+        });
+
+        executor.shutdown(); // Gracefully shutdown the executor after tasks finish
+    }
 
     void downloadFile(String fileURL, String numbers, String downloadDirectory) throws IOException {
         URL url = new URL(fileURL);
@@ -91,7 +108,7 @@ public class DownloadController {
             }
 
             InputStream inputStream = httpConn.getInputStream();
-            String saveFilePath = System.getProperty("user.dir") + "/" + downloadDirectory + "/" + fileName;
+            String saveFilePath = downloadDirectory + "/" + fileName;
 
             FileOutputStream outputStream = new FileOutputStream(saveFilePath);
 
@@ -105,7 +122,6 @@ public class DownloadController {
             inputStream.close();
 
             System.out.println("Downloading map for route " + numbers + ": File downloaded to " + saveFilePath);
-
         } else {
             System.out.println("Downloading map for route " + numbers + ": No file to download. Server replied HTTP code: " + responseCode);
         }
--- src/main/java/downloader/downloadfiles/HelloApplication.java
+++ src/main/java/downloader/downloadfiles/HelloApplication.java
@@ -2,24 +2,62 @@ package downloader.downloadfiles;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.security.spec.ECField;
+import java.util.Objects;
 
 public class HelloApplication extends Application {
+
+    private static String[] savedArgs;
+
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("download-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Map Downloader");
-        stage.setScene(scene);
-        stage.show();
+    public void init() throws Exception {
+        super.init();
+        savedArgs = getParameters().getRaw().toArray(new String[0]);
     }
 
-    public static void main(String[] args) {
-        launch();
+    @Override
+    public void start(Stage stage) throws IOException
+    {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("download-view.fxml"));
+        Parent root = fxmlLoader.load();
+        // Get the controller and call methods based on arguments
+        DownloadController dc = fxmlLoader.getController();
+        if (savedArgs.length > 0) {
+            switch (savedArgs[0]) {
+                case "NWTB":
+                    dc.downloadNWTB();
+                    break;
+                case "current":
+                    dc.downloadCurrent();
+                    break;
+                case "old":
+                    dc.downloadOlder();
+                    break;
+                case "all":
+                    dc.downloadAll();
+                    break;
+                default:
+                    System.out.println("Invalid argument: " + savedArgs[0]);
+            }
+        }
+        else
+        {
+            Scene scene = new Scene(root, 320, 240);
+            stage.setTitle("Map Downloader");
+            stage.setScene(scene);
+            stage.show();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        launch(args);
     }
 
 
