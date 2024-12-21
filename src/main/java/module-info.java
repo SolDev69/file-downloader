8c885dbc0356eadee81a228a7ccf25e08d80022f
SolDev69 <michaelcraft1104@gmail.com>
2024-12-20 23:45:33 -0500

    mend

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module downloader.downloadfiles {
     requires javafx.controls;
     requires javafx.fxml;
 
+
     opens downloader.downloadfiles to javafx.fxml;
     exports downloader.downloadfiles;
 }
